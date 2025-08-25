f5795daad0c0aa7396dd9eaaef6e0e605c3616f2
BrunoOller <brunobrunelli4@gmail.com>
2025-08-25 20:22:26 -0300

    Arrumando um código

--- CursoEmVideo/aula07/01-operadores.php
+++ CursoEmVideo/aula07/01-operadores.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 1 - Operadores unários</title>
+    <title>Exercício 1 - Operadores ternários</title>
     <link rel="stylesheet" href="../../cssDefault/style.css">
 </head>
 <body>
--- CursoEmVideo/aula07/04-eleicoes.php
+++ CursoEmVideo/aula07/04-eleicoes.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 4 - Variável de Variável</title>
+    <title>Exercício 3 - Situação</title>
     <link rel="stylesheet" href="../../cssDefault/style.css">
 </head>
 <body>
     <div>
         <?php 
             $ano = $_GET["an"];
-            $idade = 2025 - $ano;
+            $idade = date("Y") - $ano;
             $tipo = ($idade >= 18 && $idade < 65) ? "OBRIGATÓRIO" : "NÃO OBRIGATÓRIO";
 
             echo "Quem nasceu em $ano tem idade de $idade <br> Então no seu caso o voto é $tipo";
